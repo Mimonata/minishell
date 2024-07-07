b68ba7eb354f1265d5d2213601ebf9d89128d4f0
Masha <myakoven@student.42berlin.de>
2024-07-07 19:50:20 +0200

    0.0

--- masha/masha.c
+++ masha/masha.c
@@ -6,7 +6,7 @@
 /*   By: myakoven <myakoven@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 20:02:36 by myakoven          #+#    #+#             */
-/*   Updated: 2024/07/05 20:18:08 by myakoven         ###   ########.fr       */
+/*   Updated: 2024/07/06 15:50:33 by myakoven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ git checkout -b
 do not push to master branch
 accidently made "smiley" the default branch... 
 
+git checkout origin <branch> switches you to another branch
 
 
 
