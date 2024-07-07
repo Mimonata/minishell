79145effcaa40e763d0084ef4eadb13c50d329e8
Masha <myakoven@student.42berlin.de>
2024-07-07 21:06:32 +0200

    myakoven 0.1

---
+++ masha_program/include/minishell.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myakoven <myakoven@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/07 20:12:04 by myakoven          #+#    #+#             */
+/*   Updated: 2024/07/07 21:03:12 by myakoven         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include "../libft/libft.h"
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+typedef struct s_tools
+{
+	char	**env;
+}			t_tools;
+
+#endif
---
+++ masha_program/main.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myakoven <myakoven@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/07 20:51:01 by myakoven          #+#    #+#             */
+/*   Updated: 2024/07/07 21:05:45 by myakoven         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./include/minishell.h"
+
+int	main(int argc, char **argv, char **env)
+{
+	t_tools tools;
+
+	if (argc > 1)
+	{
+		ft_putstr_fd("This program does not accept arguments\n", 2);
+	}
+	tools.env = env;
+}
