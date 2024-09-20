96d7d0d14d56ca34d263cf768dbfef3cdfda2ab6
Sabina Pitul <spitul@c3a4c1.42berlin.de>
2024-09-20 20:15:26 +0200

    just t

--- program/exec.c
+++ program/exec.c
@@ -6,7 +6,7 @@
 /*   By: spitul <spitul@student.42berlin.de >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 15:48:13 by spitul            #+#    #+#             */
-/*   Updated: 2024/09/19 20:05:05 by spitul           ###   ########.fr       */
+/*   Updated: 2024/09/20 20:14:27 by spitul           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,21 +35,21 @@ int	check_builtin(char *s)
 	if (len == 4 && ft_strncmp(s, builtins[0], len) == 0)
 		a[0] = echo();
 	else if (len == 2 && ft_strncmp(s, builtins[1], len) == 0)
-		a[1] =cd();
+		a[1] = cd();
 	else if (len == 3 && ft_strncmp(s, builtins[2], len) == 0)
 		a[2] = pwd();
 	else if (len == 6 && ft_strncmp(s, builtins[3], len) == 0)
 		a[3] = export();
-	else if (len == 5 && ft_strncmp(s, builtins[4], len) == 0)	
+	else if (len == 5 && ft_strncmp(s, builtins[4], len) == 0)
 		a[4] = unset();
-	else if (len == 3 && ft_strncmp(s, builtins[5], len) == 0)		
+	else if (len == 3 && ft_strncmp(s, builtins[5], len) == 0)
 		a[5] = env();
 	else if (len == 4 && ft_strncmp(s, builtins[6], len) == 0)
-		a[6] = ft_exit();	
+		a[6] = ft_exit();
 	return (a[0] || a[1] || a[2] || a[3] || a[4] || a[5] || a[6]);
 }
 
-char	*find_cmd(char *path, char *cmd)
+char	*check_cmd_in_path(char *path, char *cmd)
 {
 	char	*cmdpath;
 	char	*temp;
@@ -59,20 +59,26 @@ char	*find_cmd(char *path, char *cmd)
 	temp = ft_strjoin(path, "/");
 	if (!temp)
 		return (NULL);
-	cmdpath =  ft_strjoin(temp, cmd);
-	free (temp);
+	cmdpath = ft_strjoin(temp, cmd);
 	if (!cmdpath)
-		return (perror); // geht es so? 
+		return (NULL);
+	free (temp);
 	if (access(cmdpath, F_OK | X_OK) == 0)
 		return (cmdpath);
-	else
+	else if (access(cmdpath, F_OK) != 0)
 	{
+		printf("msh: command not found: %s\n", cmd->arg[0]);
 		free (cmdpath);
-		return (NULL);
+		return (NULL); //TODO error handling - do we have to always write in the strerror
+	}
+	else if (access(cmdpath, F_OK | X_OK) != 0) //insufficient rights 
+	//kommt das hier oder anderswo - ist es nur ein problem bei args?
+	{
+		printf("msh: ")
 	}
 }
 
-void	check_run_cmd(char **env, t_execcmd *cmd)
+void	check_cmd(char **env, t_execcmd *cmd)
 {
 	char	*path;
 	char	*pathcmd;
@@ -80,14 +86,23 @@ void	check_run_cmd(char **env, t_execcmd *cmd)
 	int		i;
 
 	i = 0;
-	pathcmd =  NULL;
-	if (!check_builtin())
+	pathcmd = NULL;
+	path = get_env_var(env, "PATH");
+	split_path = ft_split(path, ":");
+	while (*split_path[i])
 	{
-		path = get_env_var(env, "PATH");
-		split_path = ft_split(path, ":");
-		while (*split_path[i])
+		pathcmd = check_cmd_in_path(split_path[i], cmd->arg[0]);
+		if (pathcmd != NULL)
 		{
-			pathcmd = find_cmd(split_path[i], cmd->arg[0]);
-			if (execve())
-		}			
+			exec_cmd(pathcmd, cmd, env); //pathcmd has to be freed
+			return ;
+		}
+		i ++;
+	}
+	if (!pathcmd)
+		
+}
+void	exec_cmd(char *pathcmd, t_execcmd *cmd, char **env)
+{
+		
 }
